5cc19f1b7ba8bc4eb1d9cb8e97bbb4b73136413c
kwong <kenneth5982@gmail.com>
2023-06-19 16:39:58 -0600

    Testing Manual Merge

--- plinko/src/main/java/App.java
+++ plinko/src/main/java/App.java
@@ -12,7 +12,9 @@ public class App {
             ArrayList<DogObject> dogList = new ArrayList<DogObject>();
             System.out.print("How many dogs do you have: ");
             int dogNumber = str.nextInt();
-//asdfa
+
+            System.out.println("HEHEHEHE");
+
             for (int i=0;i<dogNumber;i++){
                 System.out.print("What breed is your dog: ");
                 breed = str.next();
