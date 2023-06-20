bd399af1ad49f302733465371a053c4b296c5e7e
kwong <kenneth5982@gmail.com>
2023-06-20 10:35:39 -0600

    Testing Testing 12315421412514213

--- plinko/src/main/java/App.java
+++ plinko/src/main/java/App.java
@@ -13,7 +13,7 @@ public class App {
             System.out.print("How many dogs do you have: ");
             int dogNumber = str.nextInt();
 
-            System.out.println("IM A LITTLE TEAPOT SHORT AND SPOUT SUMTHIONG BOUT FJIEOA SOMETUMSO");
+            System.out.println("IM A LITTLOMETUMSO");
 
             for (int i=0;i<dogNumber;i++){
                 System.out.print("What breed is your dog: ");
