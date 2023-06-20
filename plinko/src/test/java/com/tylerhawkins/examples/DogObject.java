573bbaaf38030cf4f8be3c38644a09a987b0e821
kwong <kenneth5982@gmail.com>
2023-06-20 11:02:39 -0600

    jj

--- plinko/src/test/java/com/tylerhawkins/examples/DogObject.java
+++ plinko/src/test/java/com/tylerhawkins/examples/DogObject.java
@@ -47,7 +47,7 @@ public class DogObject {
     public String getBark(){
         String bark;
         if(breed == "Chihuahua"){
-            bark = "they screallllm";
+            bark = "they scream";
         }else if(breed == "Labradoodle"){
             bark = "idk they bark like a labradoodle";
         }else if(breed == "Husky"){
